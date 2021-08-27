d94b61b941768ad7470b11f9bca08a37f0c87e37
Alex <alexbean95@gmail.com>
2021-08-27 17:07:14 -0600

    Functioning image meta tag for LinkedIN

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import routes from './routes'
 function App() {
   return (
     <div className="App">
+      <meta name="image" property="og:image" content="[Image URL here]"></meta>
       <Header/>
       {routes}
     </div>
